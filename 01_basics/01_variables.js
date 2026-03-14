71f9dc6845fd0ca695256c2790fa767fce80a8f3
Ritesh  Jha <riteshjha279@gmail.com>
2026-03-14 18:13:34 +0000

    data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail ="Ritesh123@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountState;
+/*
+prefer not use to var
+becouse of issue in block scope and funcational scope
+
+*/
+ accountEmail="amoit12@gmail.com"
+ accountPassword="9876"
+ accountCity="Delhi"
+console.table([accountEmail,accountPassword,accountCity,accountState]);
