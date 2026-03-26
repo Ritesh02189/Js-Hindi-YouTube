79fb477c2d6307d67949a9579960e5fae785359a
Ritesh  Jha <riteshjha279@gmail.com>
2026-03-26 21:04:42 +0000

    date

---
+++ 01_basics/07_dates_time.js
@@ -0,0 +1,12 @@
+//  Dates
+
+let myDates = new Date()
+// console.log(myDates);
+// console.log(myDates.toString());
+// console.log(myDates.toDateString());
+// console.log(myDates.toLocaleDateString());
+// console.log(typeof myDates);
+
+let myCreatedDate = new Date(2024,0,23)
+console.log(myCreatedDate.toDateString());
+
